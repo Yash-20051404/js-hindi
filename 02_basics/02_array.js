5abd8ca9ea17f643ba29006ef1b2c5eb511fe792
Yash Jain <jainyash1404@gmail.com>
2025-06-14 08:19:36 +0000

    Array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,5 +1,5 @@
 const marvel_heros = ["thor","ironman", "spiderman"]
-const dc_heros = ["spiderman","flash","batman"]
+const dc_heros = ["superman","flash","batman"]
 
 // marvel_heros.push(dc_heros)
 
@@ -9,17 +9,19 @@ console.log(marvel_heros)
 const allheros = marvel_heros.concat(dc_heros)  // push use krte h to array k andr array aata hai isliye concat use krte h
 console.log(allheros)
 
-const ALLHEROS = [...marvel_heros , ...dc_heros]    //spread operatoir
+const ALLHEROS = [...marvel_heros , ...dc_heros]    //spread operator
 console.log(ALLHEROS)
 
 const newarray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4,5]]]
 const newarray2 = newarray.flat(Infinity)
 console.log(newarray2)
 
+// jha infinity likha hai vh depth deni hoti hai like usme kitne arrays hai
+
 // saare array ko ek array me convert krke deta hai 
 
-console.log(Array.isArray("yash"))   //return true or false on whether yash is array
-console.log(Array.from("yash"))
+console.log(Array.isArray("yash")  //return true or false on whether yash is array
+console.log(Array.from("yash"))    // returns a array from yash 
 
 console.log(Array.from({name: "yash"}))
 
